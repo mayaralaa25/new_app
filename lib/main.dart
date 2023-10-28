a31c1a383fdfd6ceb5f7021c444e7aaa69804639
mayaralaa25 <mayar.alaa252@gmail.com>
2023-10-28 23:49:03 +0200

    edit app name

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_app/layout/cubit/states.dart';
-import 'package:news_app/shared/cubit/cubit.dart';
-import 'package:news_app/shared/cubit/states.dart';
 import 'package:news_app/shared/network/local/cache_helper.dart';
 import 'package:news_app/shared/network/remote/dio_helper.dart';
 import 'package:news_app/shared/styles/themes.dart';
